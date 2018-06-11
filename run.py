d022f75569247545943db06c10547c91501a5291
PeedrinZangw <39870039+PeedrinZangw@users.noreply.github.com>
2018-06-11 23:55:51 +0000

    Add files via upload

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ TOKEN = secreto.seu_token()
 async def on_ready():
     print(client.user.name)
     print("Bot online - Olá Mundo!")
+	await client.change_status(game=discord.Game(name='Lofi Hip-Hop 24 Horas'))
 
 @client.event
 async def on_message(message):
@@ -19,16 +20,12 @@ async def on_message(message):
       except discord.errors.InvalidArgument:
              await client.send_message(message.channel, "```Entrando no Canal de VOZ...```")
 
-    if message.content.startswith('!sair'):
+    if message.content.startswith('s1!sair'):
       try:
         canaldevoz = client.voice_client_in(message.server)
         await canaldevoz.disconnect()
       except AttributeError:
           await client.send_message(message.channel,"```ATENÇÃO: O BOT Não está conectado à nenhum canal de VOZ...```")
-		  
-@client.event
-async def wait_until_login():
-    await client.change_status(game=discord.Game(name='Lofi Hip-Hop 24 Horas'))
 
 
 client.run(TOKEN)
