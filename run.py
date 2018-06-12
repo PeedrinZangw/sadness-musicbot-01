0328e794905a47e0d474c65634358a4fc9a1c245
PeedrinZangw <39870039+PeedrinZangw@users.noreply.github.com>
2018-06-12 01:35:23 +0000

    Add files via upload

--- run.py
+++ run.py
@@ -14,29 +14,44 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('s1!entrar'):
-      try:
-        canal = message.author.voice.voice_channel
-        await client.join_voice_channel(canal)
-      except discord.errors.InvalidArgument:
-             await client.send_message(message.channel, "```Entrando no Canal de VOZ...```")
+        try:
+            channel = message.author.voice.voice_channel
+            await client.join_voice_channel(channel)
+        except discord.errors.InvalidArgument:
+            await client.send_message(message.channel, "```ERRO: Nenhum canal de voz encontrado.```")
+        except Exception as error:
+            await client.send_message(message.channel, "```ERRO: {error}```".format(error=error))
 
     if message.content.startswith('s1!sair'):
-      try:
-        canaldevoz = client.voice_client_in(message.server)
-        await canaldevoz.disconnect()
-      except AttributeError:
-             await client.send_message(message.channel,"```ATENÇÃO: O BOT Não está conectado à nenhum canal de VOZ...```")
+        try:
+            voice_client = client.voice_client_in(message.server)
+            await voice_client.disconnect()
+        except AttributeError:
+            await client.send_message(message.channel, "```ERRO: Eu não estou conectado em nenhuma sala de VOZ neste momento.```")
+        except Exception as Hugo:
+            await client.send_message(message.channel, "```ERRO: {haus}```".format(haus=Hugo))
 
     if message.content.startswith('s1!tocar'):
-      try:
+        try:
             yt_url = message.content[6:]
             channel = message.author.voice.voice_channel
             voice = await client.join_voice_channel(channel)
             player = await voice.create_ytdl_player(yt_url)
             players[message.server.id] = player
             player.start()
-      except:
+        except:
             await client.send_message(message.channel, "```Erro.```")
+
+    if message.content.startswith('s1!pausar'):
+        try:
+            players[message.server.id].pause()
+        except:
+            pass
+    if message.content.startswith('s1!resume'):
+        try:
+            players[message.server.id].resume()
+        except:
+            pass
 
 
 client.run(TOKEN)
