fedeb6925845de54fdd14515bef3a8ab5a271d1e
PeedrinZangw <39870039+PeedrinZangw@users.noreply.github.com>
2018-06-12 00:42:03 +0000

    Add files via upload

--- run.py
+++ run.py
@@ -1,38 +1,138 @@
 import discord
+import secreto
 import asyncio
 import random
-import secreto
+from discord.ext import commands
 
 client = discord.Client()
 TOKEN = secreto.seu_token()
-DEIN_USERNAME = "376830307321511966"
 
 @client.event
 async def on_ready():
-    print('BOT ONLINE!')
     print(client.user.name)
-    print(client.user.id)
-    print('-----------')
-    await client.change_presence(game=discord.Game(name="Lofi Hip-Hop 24 Horas"))
-
+    print("Bot online - Olá Mundo!")
 
 @client.event
 async def on_message(message):
+    if message.content.startswith('s1!entrar'):
+      try:
+        canal = message.author.voice.voice_channel
+        await client.join_voice_channel(canal)
+      except discord.errors.InvalidArgument:
+             await client.send_message(message.channel, "```Entrando no Canal de VOZ...```")
+
+    if message.content.startswith('s1!sair'):
+      try:
+        canaldevoz = client.voice_client_in(message.server)
+        await canaldevoz.disconnect()
+      except AttributeError:
+          await client.send_message(message.channel,"```ATENÇÃO: O BOT Não está conectado à nenhum canal de VOZ...```")
+		  
     if message.content.lower().startswith('s1!teste'):
         await client.send_message(message.channel, "```Mensagem de TESTE...```")
+		
+@commands.command(pass_context=True, no_pm=True)
+    async def play(self, ctx, *, song : str):
+        state = self.get_voice_state(ctx.message.server)
+        opts = {
+            'default_search': 'auto',
+            'quiet': True,
+        }
 
-    if message.content.lower().startswith('s1!moeda'): #Coinflip 50/50% chance kopf oder zahl
-        choice = random.randint(1,2)
-        if choice == 1:
-            await client.add_reaction(message, '??')
-        if choice == 2:
-            await client.add_reaction(message, '??')
+        if state.voice is None:
+            success = await ctx.invoke(self.summon)
+            await self.bot.say("Loading the song please be patient..")
+            if not success:
+                return
 
-    if message.content.startswith('s1!status') and message.author.id == DEIN_USERNAME:
-        game = message.content[6:]
-        await client.change_presence(game=discord.Game(name=game))
-        await client.send_message(message.channel, "Meu status foi alterado para " + game + "")
+        try:
+            player = await state.voice.create_ytdl_player(song, ytdl_options=opts, after=state.toggle_next)
+        except Exception as e:
+            fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
+            await self.bot.send_message(ctx.message.channel, fmt.format(type(e).__name__, e))
+        else:
+            player.volume = 0.6
+            entry = VoiceEntry(ctx.message, player)
+            await self.bot.say('Enqueued ' + str(entry))
+            await state.songs.put(entry)
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def volume(self, ctx, value : int):
+        """Sets the volume of the currently playing song."""
+
+        state = self.get_voice_state(ctx.message.server)
+        if state.is_playing():
+            player = state.player
+            player.volume = value / 100
+            await self.bot.say('Set the volume to {:.0%}'.format(player.volume))
+    @commands.command(pass_context=True, no_pm=True)
+    async def resume(self, ctx):
+        """Resumes the currently played song."""
+        state = self.get_voice_state(ctx.message.server)
+        if state.is_playing():
+            player = state.player
+            player.resume()
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def stop(self, ctx):
+        """Stops playing audio and leaves the voice channel.
+        This also clears the queue.
+        """
+        server = ctx.message.server
+        state = self.get_voice_state(server)
+
+        if state.is_playing():
+            player = state.player
+            player.stop()
+
+        try:
+            state.audio_player.cancel()
+            del self.voice_states[server.id]
+            await state.voice.disconnect()
+            await self.bot.say("Cleared the queue and disconnected from voice channel ")
+        except:
+            pass
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def skip(self, ctx):
+        """Vote to skip a song. The song requester can automatically skip.
+        3 skip votes are needed for the song to be skipped.
+        """
+
+        state = self.get_voice_state(ctx.message.server)
+        if not state.is_playing():
+            await self.bot.say('Not playing any music right now...')
+            return
+
+        voter = ctx.message.author
+        if voter == state.current.requester:
+            await self.bot.say('Requester requested skipping song...')
+            state.skip()
+        elif voter.id not in state.skip_votes:
+            state.skip_votes.add(voter.id)
+            total_votes = len(state.skip_votes)
+            if total_votes >= 3:
+                await self.bot.say('Skip vote passed, skipping song...')
+                state.skip()
+            else:
+                await self.bot.say('Skip vote added, currently at [{}/3]'.format(total_votes))
+        else:
+            await self.bot.say('You have already voted to skip this song.')
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def playing(self, ctx):
+        """Shows info about the currently played song."""
+
+        state = self.get_voice_state(ctx.message.server)
+        if state.current is None:
+            await self.bot.say('Not playing anything.')
+        else:
+            skip_count = len(state.skip_votes)
+            await self.bot.say('Now playing {} [skips: {}/3]'.format(state.current, skip_count))
+            
+def setup(bot):
+    bot.add_cog(Music(bot))
+    print('Music is loaded')
 
 
-
-client.run('TOKEN')
+client.run(TOKEN)
